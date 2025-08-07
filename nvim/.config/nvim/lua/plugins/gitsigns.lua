d6f62616825b4f90e92f6474edfe498f20c64dd9
Julian Ray <julian.r8y@gmail.com>
2025-08-07 16:47:54 -0400

    Go to tokyonight-night

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,3 +1,19 @@
 return {
     "lewis6991/gitsigns.nvim",
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {
+        signcolumn = true,
+    },
+    keys = {
+        {
+            "<leader>gtb",
+            function() require("gitsigns").toggle_current_line_blame() end,
+            desc = "Toggle inline git blame",
+        },
+        {
+            "<leader>gb",
+            function() require("gitsigns").blame() end,
+            desc = "Open side bar with git blame",
+        },
+    },
 }
