46fba847663bde203e4a78ae0e6002a4e4afb168
Julian Ray <julian.r8y@gmail.com>
2025-11-13 00:34:47 -0500

    Update some snacks keybinds

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -101,13 +101,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
       vim.keymap.set(mode, lhs, rhs, opts)
     end
 
-    bufmap("n", "gd", function() vim.lsp.buf.definition() end, 'Go to definition')
-    bufmap("n", "gt", "<cmd>tab split | lua vim.lsp.buf.definition()<CR>", "Go to definition in new tab")
-
     bufmap("n", "K", function() vim.lsp.buf.hover() end, 'Open hover')
-    bufmap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, 'Workspace symbol')
     bufmap("n", "<leader>vca", function() vim.lsp.buf.code_action() end, 'Code actions')
-    bufmap("n", "<leader>vrr", function() vim.lsp.buf.references() end, 'References')
     bufmap("n", "<leader>vrn", function() vim.lsp.buf.rename() end, 'Rename')
     bufmap('n', '<C-j>', function() vim.diagnostic.open_float() end, 'Open float')
     bufmap("n", "[d", function() vim.diagnostic.goto_prev() end, 'Go to prev diagnostic')
