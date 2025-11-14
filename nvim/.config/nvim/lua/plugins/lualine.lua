2c810bd9ce7dda875ef422950d2f78ee65c8d4d5
Julian Ray <julian.r8y@gmail.com>
2025-11-14 17:54:45 -0500

    Add todo for removing time module in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- TODO: Remove the time module
     opts = function()
         local colo = vim.g.colors_name
         if colo:find("catppuccin") then
