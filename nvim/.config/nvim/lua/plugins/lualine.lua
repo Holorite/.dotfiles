f09ca569250b66ab8bc74477c52284f79a62ee36
Julian Ray <julian.r8y@gmail.com>
2025-11-16 12:41:31 -0500

    Remove lualine time module for catppuccin theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,6 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    -- TODO: Remove the time module
     opts = function()
         local colo = vim.g.colors_name
         if colo:find("catppuccin") then
@@ -13,17 +12,12 @@ return {
                 sections = {
                     lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
                     lualine_b = { 'branch', 'diagnostics' },
-                    lualine_y = {
-                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-                        { "location", padding = { left = 0, right = 1 } },
-                    },
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'diff' },
+                    lualine_y = { 'filetype' },
                     lualine_z = {
-                        {
-                            function()
-                                return " " .. os.date("%R")
-                            end,
-                            separator = { right = "" },
-                        },
+                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                        { "location", padding = { left = 0, right = 0 }, separator = { right = "" } },
                     },
                 },
             }
