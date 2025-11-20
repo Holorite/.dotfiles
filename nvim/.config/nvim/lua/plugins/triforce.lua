2a669b05a3e1b18adbe134958b00ee57fb2b0d51
Julian Ray <julian.r8y@gmail.com>
2025-11-20 00:02:45 -0500

    add triforce

---
+++ nvim/.config/nvim/lua/plugins/triforce.lua
@@ -0,0 +1,12 @@
+return {
+    "gisketch/triforce.nvim",
+    dependencies = {
+        "nvzone/volt",
+    },
+    opts = {
+        gamification_enabled = true,
+    },
+    keys = {
+        { "<leader>tp", mode = "n", function() require('triforce').show_profile() end, desc = "Show Triforce Profile" }
+    }
+}
