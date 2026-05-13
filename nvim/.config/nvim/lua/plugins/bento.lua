98fd390a531beee4d67580e130cedfe3f79b1f67
Julian Ray <julian.r8y@gmail.com>
2026-05-13 08:13:08 -0700

    Load bento on >1 buf and set max buffers to 6

--- nvim/.config/nvim/lua/plugins/bento.lua
+++ nvim/.config/nvim/lua/plugins/bento.lua
@@ -1,6 +1,20 @@
 return {
     'serhez/bento.nvim',
+    lazy = true,
+    init = function()
+        vim.api.nvim_create_autocmd("BufAdd", {
+            callback = function()
+                if #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
+                    require("bento")
+                    return true
+                end
+            end,
+        })
+    end,
     opts = {
+        max_open_buffers = 6,
+        buffer_deletion_metric = "frecency_access",
+        buffer_notify_on_delete = true,
         ui = {
             floating = {
                 minimal_menu = 'dashed',
