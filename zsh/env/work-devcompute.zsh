e250e8a144c0de2bf0f2a8b8c8c4fa1683651e58
Julian Ray <julian.r8y@gmail.com>
2026-07-17 08:34:18 -0700

    Lemonade command improvements

--- zsh/env/work-devcompute.zsh
+++ zsh/env/work-devcompute.zsh
@@ -7,7 +7,7 @@ hash -d envs=/local/mnt/workspace/juliray/envs
 pyenv() {
     local env_name="$1"
     if [ -z "$env_name" ]; then
-        echo "Usage: source-env <env_name>"
+        echo "Usage: pyenv <env_name> | pyenv list | pyenv create <env_name>"
         return 1
     fi
 
@@ -15,6 +15,23 @@ pyenv() {
     if [ $env_name = "list" ]; then
         echo "Available environments:"
         ls "$env_base"
+        return 0
+    fi
+
+    if [ $env_name = "create" ]; then
+        local new_name="$2"
+        if [ -z "$new_name" ]; then
+            echo "Usage: pyenv create <env_name>"
+            return 1
+        fi
+        local new_path="$env_base/$new_name"
+        if [ -d "$new_path" ]; then
+            echo "Environment '$new_name' already exists in $envs"
+            return 1
+        fi
+        python3 -m venv "$new_path" || return 1
+        source "$new_path/bin/activate"
+        echo "Created and activated environment: $new_name"
         return 0
     fi
 
